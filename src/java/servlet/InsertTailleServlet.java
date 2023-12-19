c3f5f110fa17fc3c6c2521f18cc9849e6ecc9438
NalyAndriam <153374026+NalyAndriam@users.noreply.github.com>
2023-12-19 16:57:08 +0300

    creationMeuble

--- src/java/servlet/InsertTailleServlet.java
+++ src/java/servlet/InsertTailleServlet.java
@@ -16,7 +16,7 @@ import java.io.*;
 import java.sql.*;
 
 @WebServlet("/insertTaille")
-public class InsertStyleServlet extends HttpServlet {
+public class InsertTailleServlet extends HttpServlet {
     
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
         throws ServletException, IOException {
@@ -45,7 +45,7 @@ public class InsertStyleServlet extends HttpServlet {
         } catch (Exception e) {
             e.printStackTrace();
             // Rediriger vers une page d'erreur avec le message d'erreur approprié
-            request.setAttribute("errorMessage", "Une erreur s'est produite lors de l'insertion du style : " + e.getMessage());
+            request.setAttribute("errorMessage", "Une erreur s'est produite lors de l'insertion de la taille : " + e.getMessage());
             RequestDispatcher rd = request.getRequestDispatcher("WEB-INF/error.jsp");
             rd.forward(request, response);
             // Gérer les erreurs SQL (par exemple, afficher un message d'erreur)
--- web/WEB-INF/insertTaille.jsp
+++ web/WEB-INF/insertTaille.jsp
@@ -9,9 +9,9 @@
         <!-- <div id="header1"></div> -->
     <center>
         <div id="all">
-        <form method="get" action="index.jsp">
+        <form method="post" action="insertTaille">
             <h1>Ins&eacute;rez la taille</h1>
-            <input type="text" name="style" class="form-control">
+            <input type="text" name="taille" class="form-control">
             <input type="submit" value="Inserer">
         </form>
         </div>
