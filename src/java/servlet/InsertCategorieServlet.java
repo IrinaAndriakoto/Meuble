cbc8251f55d597340fefeebbacaedba6cef4c336
NalyAndriam <153374026+NalyAndriam@users.noreply.github.com>
2023-12-19 08:40:02 +0300

    update listeStyle

---
+++ src/java/servlet/InsertCategorieServlet.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package servlet;
+
+/**
+ *
+ * @author Irina
+ */
+import inc.Service;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import java.io.*;
+import java.sql.*;
+
+public class InsertStyleServlet extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String categorie = request.getParameter("categorie");
+
+        Connection connection = null;
+        
+        try {
+            // Établir la connexion à la base de données (assurez-vous d'avoir correctement configuré votre source de données / pool de connexions)
+            // ...
+            Service dataService = new Service();
+            connection = dataService.getConnection();
+            request.setAttribute("connection",connection);
+            dataService.insertCategorie(connection, categorie);
+           
+
+            // Fermer la connexion
+            connection.close();
+
+            // Rediriger vers une page de confirmation ou une autre page appropriée
+            response.sendRedirect("confirmation.jsp");
+        } catch (Exception e) {
+            e.printStackTrace();
+            // Rediriger vers une page d'erreur avec le message d'erreur approprié
+            request.setAttribute("errorMessage", "Une erreur s'est produite lors de l'insertion de la categorie : " + e.getMessage());
+            RequestDispatcher rd = request.getRequestDispatcher("error.jsp");
+            rd.forward(request, response);
+            // Gérer les erreurs SQL (par exemple, afficher un message d'erreur)
+        } finally {
+            // Assurez-vous de bien fermer la connexion dans le bloc finally
+            try {
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
--- web/WEB-INF/listeStyle.jsp
+++ web/WEB-INF/listeStyle.jsp
@@ -11,7 +11,8 @@
 <head>
     <title>Styles</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/css/style.css" rel="stylesheet">
 </head>
 <body>
     <h2>Styles :</h2>
